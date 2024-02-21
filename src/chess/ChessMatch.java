2ee70e8d09747ce9ffc4dd962a1f6eb54789d6df
FernandoJMartins <julio8martins@gmail.com>
2024-02-21 00:52:41 -0300

    Defensive Programming and Board Exception

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -37,5 +37,6 @@ public class ChessMatch {
         board.placePiece(new Rook(board, Color.BLACK), new Position(7,7));
         board.placePiece(new Rook(board, Color.BLACK), new Position(7,0));
         board.placePiece(new King(board, Color.BLACK), new Position(7,4));
+        board.placePiece(new King(board, Color.BLACK), new Position(7,7));
     }
 }
